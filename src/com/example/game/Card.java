e77e520ee1d78da08ee5aca6aa1ae9278ebb84b5
Krin <372137241@qq.com>
2021-05-31 21:14:34 +0800

    引包

--- src/com/example/game/Card.java
+++ src/com/example/game/Card.java
@@ -1,7 +1,43 @@
 package com.example.game;
 
+import android.content.Context;
+import android.view.Gravity;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 public class Card extends FrameLayout {
 
+	public Card(Context context) {
+		super(context);
+	
+		 lab = new TextView(getContext());
+		lab.setTextSize(32);
+		lab.setBackgroundColor(0x33ffffff);
+		lab.setGravity(Gravity.CENTER);
+		LayoutParams lp = new LayoutParams(-1,-1);
+		lp.setMargins(10, 10, 0, 0);
+		addView(lab,lp);
+		
+		setNum(0);
+	}
+
+	private int num = 0;
+	public int getNum(){
+		return num;
+	}
+	
+	public void setNum(int num){
+		this.num = num;
+		if(num<=0){
+			lab.setText("");
+		}else{
+			lab.setText(num+"");
+		}
+	}
+	public boolean equals(Card o){
+		return getNum()==o.getNum();
+	}
+	
+private TextView lab;
 }
+
--- src/com/example/game/MainActivity.java
+++ src/com/example/game/MainActivity.java
@@ -4,14 +4,19 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
 
 public class MainActivity extends Activity {
+	public MainActivity(){
+		mainActivity=this;
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
+		tvScore=(TextView) findViewById(R.id.tvScore);	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -19,16 +24,21 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+	public void clearScore(){
+		Score=0;
+		showScore();
+	}
+	public void showScore(){
+		tvScore.setText(Score+"");
+	}
+	public void addScore(int s){
+		Score+=s;
+		showScore();
+	}
+	private int Score = 0;
+	private TextView tvScore;
+	private static MainActivity mainActivity =null;
+	public static MainActivity getMainActivity(){
+		return mainActivity;
 	}
 }
