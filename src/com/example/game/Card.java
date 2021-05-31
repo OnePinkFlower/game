9c7e9c7386675e34ffcd4516690d3c313139e86b
weixingxiang <372137241@qq.com>
2021-05-31 12:59:06 +0800

    初始化

---
+++ src/com/example/game/Card.java
@@ -0,0 +1,7 @@
+package com.example.game;
+
+import android.widget.FrameLayout;
+
+public class Card extends FrameLayout {
+
+}
---
+++ src/com/example/game/GameView.java
@@ -0,0 +1,7 @@
+package com.example.game;
+
+import android.widget.GridLayout;
+
+public class GameView extends GridLayout {
+
+}
